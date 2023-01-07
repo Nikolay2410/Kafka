ee79838b58843a24edfd32d78e376be51a113cb7
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2023-01-07 18:11:10 +0800

    💦 :)

--- src/main.js
+++ src/main.js
@@ -1,72 +1,46 @@
-// const cors = require("cors");
-// const express = require("express");
+const cors = require("cors");
+const express = require("express");
 const { Kafka } = require("kafkajs");
-// const app = express();
+
+const PORT = 3000;
 
 const kafka = new Kafka({
   clientId: "kafka",
   brokers: ["localhost:9092"],
 });
-const consumer = kafka.consumer({ groupId: "test-group" });
+const consumer = kafka.consumer({ groupId: "my-group" });
 
-// app.use(cors());
+const app = express();
+app.use(cors());
 
-async function start() {
-  try {
-    const messages = [];
-    await consumer.connect();
-    await consumer.subscribe({ topic: "YarmarkaTopic", fromBeginning: true });
-    await new Promise(async (resolve) => {
-      let timeout = setTimeout(resolve, 2000);
-      //
-      console.log("consumer подписался на eachMessage");
-      //
-      await consumer.run({
-        eachMessage: ({ topic, partition, message }) => {
-          //
-          console.log("Сообщение пришло");
-          console.log(message);
-          //
-          clearTimeout(timeout);
-          timeout = setTimeout(resolve, 2000);
-          messages.push(message.value.toString());
-        },
-      });
+async function consumeAllMessages() {
+  const messages = [];
+  await consumer.connect();
+  await consumer.subscribe({ topic: "YarmarkaTopic", fromBeginning: true });
+  await new Promise(async (resolve) => {
+    let timeout = setTimeout(resolve, 1000);
+    consumer.run({
+      eachMessage: ({ topic, partition, message }) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(resolve, 1000);
+        messages.push(message.value.toString());
+      },
     });
-    //
-    console.log("consumer отписался");
-    //
-    await consumer.disconnect();
-    console.log(messages);
-    // res.send(messages);
-  } catch (error) {
-    console.log(error);
-    // res.send(error).status(400);
-  }
+    consumer.seek({ topic: "YarmarkaTopic", partition: 0, offset: 1000 });
+  });
+  await consumer.disconnect();
+  return messages;
 }
 
-start();
+app.get("/", async function (req, res) {
+  try {
+    const messages = await consumeAllMessages();
+    res.send(messages);
+  } catch (error) {
+    res.send(error).status(400);
+  }
+});
 
-// app.get("/", async function (req, res) {
-//   try {
-//     const messages = [];
-//     await consumer.connect();
-//     await consumer.subscribe({ topic: "YarmarkaTopic", fromBeginning: true });
-//     await new Promise(async (res) => {
-//       let timeout = setTimeout(res, 2000);
-//       await consumer.run({
-//         eachMessage: ({ topic, partition, message }) => {
-//           clearTimeout(timeout);
-//           timeout = setTimeout(res, 2000);
-//           messages.push(message.value.toString());
-//         },
-//       });
-//     });
-//     await consumer.disconnect();
-//     res.send(messages);
-//   } catch (error) {
-//     res.send(error).status(400);
-//   }
-// });
-
-// app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`server started https://localhost:${PORT}`);
+});
