b9f28c05ea0a112b48fb2903de9f6986bd4b601c
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2023-01-07 16:13:36 +0800

    🆕 добавил setTimeout для ожидания сообщений

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 const cors = require("cors");
 const express = require("express");
-const { exec } = require("child_process");
-const app = express();
-
 const { Kafka } = require("kafkajs");
+const app = express();
 
 const kafka = new Kafka({
   clientId: "kafka",
@@ -14,18 +12,27 @@ const consumer = kafka.consumer({ groupId: "test-group" });
 app.use(cors());
 
 app.get("/", async function (req, res) {
-  await consumer.connect();
-  await consumer.subscribe({ topic: "YarmarkaTopic", fromBeginning: true });
+  try {
+    await consumer.connect();
+    await consumer.subscribe({ topic: "YarmarkaTopic", fromBeginning: true });
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
-      console.log(`- ${prefix} ${message.key}#${message.value}`)
-      res.send({
-        value: message.value.toString(),
+    const messages = [];
+
+    await new Promise(async (res) => {
+      let timeout = setTimeout(res, 1000);
+      await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+          clearTimeout(timeout);
+          timeout = setTimeout(res, 1000);
+          messages.push(message.value.toString());
+        },
       });
-    },
-  });
+    });
+    await consumer.disconnect();
+    res.send(messages);
+  } catch (error) {
+    res.send(error).status(400);
+  }
 });
 
 app.listen(3000);
