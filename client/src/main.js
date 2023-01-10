3bdf043f7825c0fe7ef516e4f9d40b6f2937a699
root <root@DebianNew.myguest.virtualbox.org>
2023-01-10 18:57:52 +0800

    Добавил вывод json

--- client/src/main.js
+++ client/src/main.js
@@ -1,33 +1,18 @@
 async function fetchMessages() {
-  const res = await fetch("http://localhost:3000/api/messages");
+  const res = await fetch("/api/messages");
   if (!res.ok) throw new Error(res.statusText);
   return res.json();
 }
 
-const btnProjects = document.getElementById("btn");
+const btnEl = document.getElementById("btn");
 const titleEl = document.getElementById("title");
 const messagesListEl = document.getElementById("messages");
-const btnCandidates = document.getElementById("btn");
 
-btnProjects.addEventListener("click", async () => {
+btnEl.addEventListener("click", async () => {
   try {
-    titleEl.textContent = "Проекты";
+    titleEl.textContent = "Сообщения";
     const messages = await fetchMessages();
-    messagesListEl.innerHTML = messages
-      .map((message) => `<li>${message}</li>`)
-      .join("");
-  } catch (error) {
-    titleEl.textContent = String(error);
-  }
-});
-
-btnCandidates.addEventListener("click", async () => {
-  try {
-    titleEl.textContent = "Студент";
-    const messages = await fetchMessages();
-    messagesListEl.innerHTML = messages
-      .map((message) => `<li>${message}</li>`)
-      .join("");
+    messagesListEl.innerHTML = JSON.stringify(messages);
   } catch (error) {
     titleEl.textContent = String(error);
   }
