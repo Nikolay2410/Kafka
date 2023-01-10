61ef74ac9e768d6fee036861841dd67080a7baf1
Nikolay2410 <gorbenkonikolay024>
2023-01-10 15:24:00 +0800

    Подправил кнопки

--- client/src/main.js
+++ client/src/main.js
@@ -4,13 +4,26 @@ async function fetchMessages() {
   return res.json();
 }
 
-const btnEl = document.getElementById("btn");
+const btnProjects = document.getElementById("btn");
 const titleEl = document.getElementById("title");
 const messagesListEl = document.getElementById("messages");
+const btnCandidates = document.getElementById("btn");
 
-btnEl.addEventListener("click", async () => {
+btnProjects.addEventListener("click", async () => {
   try {
-    titleEl.textContent = "Сообщения";
+    titleEl.textContent = "Проекты";
+    const messages = await fetchMessages();
+    messagesListEl.innerHTML = messages
+      .map((message) => `<li>${message}</li>`)
+      .join("");
+  } catch (error) {
+    titleEl.textContent = String(error);
+  }
+});
+
+btnCandidates.addEventListener("click", async () => {
+  try {
+    titleEl.textContent = "Студент";
     const messages = await fetchMessages();
     messagesListEl.innerHTML = messages
       .map((message) => `<li>${message}</li>`)
